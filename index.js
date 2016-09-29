e3e564e0a6c11dbe5aaf835e9c3312e54c69d9e8
Matt Oates <matt@boardom.io>
2016-09-29 15:38:25 -0700

    PORT

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/', function (req, res) {
     `)
 });
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
     console.log('listening on', port);
